9cc01066e813d9944bdac695e82a27b1ddb8269e
Programmer Sudip <sudipmicrosoft259@gmail.com>
2024-10-19 22:40:34 +0600

    list

---
+++ main.py
@@ -0,0 +1,21 @@
+# Program to show Constant Time Complexity
+
+def printnumber(n):
+    iterations=0
+    print("The number entered by the user is:", n)
+    iterations+=1
+    print("Total iterations done by the code is ", iterations,"\n")
+
+
+printnumber(10)
+printnumber(20)
+printnumber(30)
+print("\nWith any 'n' the time taken by the code won't change" )
+
+
+
+
+
+
+
+
